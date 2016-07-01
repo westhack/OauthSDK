36721b265765062dab045094d9cab36fff18637d
github <452855489@qq.com>
2016-07-01 17:04:53 +0800

    ''

--- examples/config.php
+++ examples/config.php
@@ -5,18 +5,18 @@ $config =  array(
     'QQ' => array(
         'APP_KEY' => '123456', //应用注册成功后分配的 APP ID
         'APP_SECRET' => '9cc9ac2fb17d010104d8a58dbebb4d3a', //应用注册成功后分配的KEY
-        'CALLBACK' =>  'http://localhost/login.php?type=qq',//回调URL
+        'CALLBACK' =>  'http://localhost/callback.php?type=qq',//回调URL
     ),
     //新浪微博配置
     'SINA' => array(
         'APP_KEY' => '123456', //应用注册成功后分配的 APP ID
         'APP_SECRET' => '9cc9ac2fb17d010104d8a58dbebb4d3a', //应用注册成功后分配的KEY
-        'CALLBACK' => 'http://localhost/login.php?type=sina',//回调URL
+        'CALLBACK' => 'http://localhost/callback.php?type=sina',//回调URL
     ),
     //腾讯微信配置
     'WECHAT' => array(
         'APP_KEY' => '123456', //应用注册成功后分配的 APP ID
         'APP_SECRET' => '9cc9ac2fb17d010104d8a58dbebb4d3a', //应用注册成功后分配的KEY
-        'CALLBACK' => 'http://localhost/login.php?type=wechat',//回调URL
+        'CALLBACK' => 'http://localhost/callback.php?type=wechat',//回调URL
     )
 );
